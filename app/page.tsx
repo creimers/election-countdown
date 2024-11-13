8e96d20d4b561ddc7c52904ad68010080c4d8ab7
Christoph Reimers <christoph@superservice-international.com>
2024-11-13 11:33:49 +0100

    fix z index

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="bg-[#005437] flex justify-center py-24">
       <div className="w-2/3 2xl:w-1/2 relative p-8 pl-12 text-[#F5F1E9]">
         <Countdown targetDate={"2025-02-23"} />
-        <div className="absolute inset-0 -z-10 bg-[#008939] -skew-x-12"></div>
+        <div className="absolute inset-0 z-0 bg-[#008939] -skew-x-12"></div>
       </div>
     </div>
   );
